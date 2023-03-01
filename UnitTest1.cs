6122d633b17596c2f88545605050f7e4164024e9
vijay <vijaydurkikar1@gmail.com>
2023-03-01 14:53:56 +0530

    [vijay]Add:User Need To Enter Valid Last Name in Test Case-2

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -12,5 +12,12 @@ namespace TestingRegex
             string actualOutput = regexMethod.ValidateFirstName("Vijay");
             Assert.That(actualOutput, Is.EqualTo(expectedOutput));
         }
+        [Test]
+        public void GivenLastName_WhenAnalysed_ShouldReturnLastName()
+        {
+            string expectedOutput = "Last name matching with Regex";
+            string actualOutput = regexMethod.ValidateLastName("Durkikar");
+            Assert.That(actualOutput, Is.EqualTo(expectedOutput));
+        }
     }
 }
